cb4f9be173b7fce15a5b608ab26659c588ac3cc1
TIM88-DOT <tim@Tim>
2024-04-29 00:40:49 +0100

    raw mode

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,32 @@ use std::os::unix::io::RawFd;
 use std::slice::from_mut;
 use termios::*;
 
-
 fn main() {
     let stdin_fd = io::stdin().as_raw_fd();
-    let _ = enable_raw_mode(stdin_fd);
+    let mut termios = Termios::from_fd(stdin_fd).unwrap();
+    let original_termios = termios.clone();
+    let _ = enable_raw_mode(stdin_fd, &mut termios);
 
     let mut c: u8 = 0;
-    while io::stdin().read_exact(from_mut(&mut c)).is_ok() && c != b'q' {
+    while io::stdin().read(from_mut(&mut c)).is_ok() && c != b'q' {
         // Continue reading until 'q' is entered
+        if c.is_ascii_control() {
+            println!("{}", c);
+        } else {
+            println!("{} ('{}')", c, c as char);
+        }
     }
+
+    disable_raw_mode(stdin_fd, &original_termios).unwrap();
 }
 
-fn enable_raw_mode(fd : RawFd) -> Result<()> {
-    let mut termios = Termios::from_fd(fd).unwrap();
-    termios.c_lflag = !(ECHO);
-    tcsetattr(fd, TCSANOW, &termios).unwrap();
+fn disable_raw_mode(fd: RawFd, original_termios: &Termios) -> Result<()> {
+    tcsetattr(fd, TCSAFLUSH, original_termios)?;
     Ok(())
 }
 
-
+fn enable_raw_mode(fd: RawFd, termios: &mut Termios) -> Result<()> {
+    termios.c_lflag &= !(ECHO | ICANON);
+    tcsetattr(fd, TCSAFLUSH, termios)?;
+    Ok(())
+}
