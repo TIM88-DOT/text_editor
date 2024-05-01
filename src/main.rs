6658faaac21f2829ce99087415e25a80073f5f49
TIM88-DOT <tim@Tim>
2024-05-01 18:46:30 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let stdin_fd = io::stdin().as_raw_fd();
     let mut termios = Termios::from_fd(stdin_fd).unwrap();
     let original_termios = termios.clone();
-    let _ = enable_raw_mode(stdin_fd, &mut termios);
+    let _ = enable_raw_mode(stdin_fd, &mut termios).unwrap();
 
     loop {
         let mut c: u8 = 0;
@@ -27,21 +27,28 @@ fn main() {
 }
 
 fn disable_raw_mode(fd: RawFd, original_termios: &Termios) -> Result<()> {
-    tcsetattr(fd, TCSAFLUSH, original_termios)?;
+    if let Err(err) = tcsetattr(fd, TCSAFLUSH, original_termios) {
+        return Err(io::Error::new(io::ErrorKind::Other, format!("tcsetattr: {}", err)));
+    }
     Ok(())
 }
 
 fn enable_raw_mode(fd: RawFd, termios: &mut Termios) -> Result<()> {
-    termios.c_iflag &= !(ICRNL | IXON);
+    if let Err(err) = tcgetattr(fd, termios) {
+        return Err(io::Error::new(io::ErrorKind::Other, format!("tcgetattr: {}", err)));
+    }
+
+    termios.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     termios.c_oflag &= !(OPOST);
+    termios.c_cflag |= CS8;
     termios.c_lflag &= !(ECHO | ICANON | ISIG | IEXTEN);
 
     termios.c_cc[VMIN] = 0;
     termios.c_cc[VTIME] = 1;
 
-    tcsetattr(fd, TCSAFLUSH, termios)?;
+    if let Err(err) =  tcsetattr(fd, TCSAFLUSH, termios) {
+        return Err(io::Error::new(io::ErrorKind::Other, format!("tcsetattr: {}", err)));
+    }
     Ok(())
 }
-
-
 
