cb90381dc1ace169b846d2e7fb3c2dcbb0defb17
TIM88-DOT <tim@Tim>
2024-05-06 05:52:36 +0100

    added refactoring and screen refresh

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,100 @@
-use std::io::{self, stdin, Read, Result};
+use std::io::{self, Read, Result, Write};
 use std::os::fd::AsRawFd;
-use std::os::unix::io::RawFd;
-use std::slice::from_mut;
 use termios::*;
 
+const fn ctrl_key(c: u8) -> u8 {
+    c & 0x1f
+}
+
+struct RawMode {
+    orig_termios: Termios,
+}
+
+impl RawMode {
+    fn new() -> Result<Self> {
+        let orig_termios = Termios::from_fd(io::stdin().as_raw_fd())?;
+        Ok(RawMode { orig_termios })
+    }
+
+    fn enable(&self) -> Result<()>{
+        let mut raw = self.orig_termios.clone();
+        raw.c_lflag &= !(ECHO);
+        raw.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+        raw.c_oflag &= !(OPOST);
+        raw.c_cflag |= CS8;
+        raw.c_lflag &= !(ECHO | ICANON | ISIG | IEXTEN);
+    
+        raw.c_cc[VMIN] = 0;
+        raw.c_cc[VTIME] = 1;
+        tcsetattr(io::stdin().as_raw_fd(), TCSAFLUSH, &raw)?;
+        Ok(())
+    }
+
+    fn disable(&self) -> Result<()> {
+        tcsetattr(io::stdin().as_raw_fd(), TCSAFLUSH, &self.orig_termios)?;
+        Ok(())
+    }
+}
+
+impl Drop for RawMode {
+    fn drop(&mut self) {
+        self.disable().unwrap();
+    }
+}
+
 fn main() {
-    let stdin_fd = io::stdin().as_raw_fd();
-    let mut termios = Termios::from_fd(stdin_fd).unwrap();
-    let original_termios = termios.clone();
-    let _ = enable_raw_mode(stdin_fd, &mut termios).unwrap();
-
+    let raw_mode = RawMode::new().unwrap();
+    raw_mode.enable().unwrap();
     loop {
-        let mut c: u8 = 0;
-        stdin().read(from_mut(&mut c)).expect("Invalid");
-        if c.is_ascii_control() {
-            println!("{}\r\n", c);
-        } else {
-            println!("{} ('{}')\r\n", c, c as char);
+        editor_refresh_screen().unwrap();
+        editor_process_keypress(&raw_mode);
+    }
+}
+
+fn editor_read_key() -> u8 {
+    loop {
+        let mut c = [0; 1];
+        match io::stdin().read(&mut c) {
+            Ok(_) => {
+                return c[0];
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
+            Err(ref e) => {
+                eprintln!("read: {}", e);
+                die("read");
+            }
         }
-        if c == b'q' {
-            break 
-        };
     }
-
-    disable_raw_mode(stdin_fd, &original_termios).unwrap();
 }
 
-fn disable_raw_mode(fd: RawFd, original_termios: &Termios) -> Result<()> {
-    if let Err(err) = tcsetattr(fd, TCSAFLUSH, original_termios) {
-        return Err(io::Error::new(io::ErrorKind::Other, format!("tcsetattr: {}", err)));
+fn editor_draw_rows() -> Result<()> {
+    for _ in 0..24 {
+        io::stdout().write_all(b"~\r\n")?;
     }
     Ok(())
 }
 
-fn enable_raw_mode(fd: RawFd, termios: &mut Termios) -> Result<()> {
-    if let Err(err) = tcgetattr(fd, termios) {
-        return Err(io::Error::new(io::ErrorKind::Other, format!("tcgetattr: {}", err)));
+fn editor_process_keypress(raw_mode : &RawMode) {
+    match editor_read_key() {
+        c if ctrl_key(c) == ctrl_key(b'x') => {
+            io::stdout().write_all(b"\x1b[2J\x1b[H").unwrap();
+            io::stdout().flush().unwrap();
+            raw_mode.disable().unwrap();
+            std::process::exit(0)},
+        _ => (),
     }
-
-    termios.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    termios.c_oflag &= !(OPOST);
-    termios.c_cflag |= CS8;
-    termios.c_lflag &= !(ECHO | ICANON | ISIG | IEXTEN);
-
-    termios.c_cc[VMIN] = 0;
-    termios.c_cc[VTIME] = 1;
-
-    if let Err(err) =  tcsetattr(fd, TCSAFLUSH, termios) {
-        return Err(io::Error::new(io::ErrorKind::Other, format!("tcsetattr: {}", err)));
-    }
-    Ok(())
 }
 
+fn editor_refresh_screen() -> io::Result<()> {
+    io::stdout().write_all(b"\x1b[2J\x1b[H")?;
+    editor_draw_rows()?;
+    io::stdout().write_all(b"\x1b[H")?;
+    io::stdout().flush()?;
+    Ok(())
+}
+fn die(s: &str) -> ! {
+    io::stdout().write_all(b"\x1b[2J\x1b[H").unwrap();
+    io::stdout().flush().unwrap();
+    eprintln!("{}", s);
+    std::process::exit(1);
+}
